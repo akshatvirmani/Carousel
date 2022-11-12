b4cdd6501279d5a5e129e2269fc545ad361f1680
Akshat Virmani <akshatvirmani93@gmail.com>
2022-11-12 19:26:02 +0530

    commit

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import Slider from "react-slick";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+
+import "./ImageSlider.css";
+
+const NextArrow = ({ onClick }) => {
+  return (
+    <div className="nextArrow" onClick={onClick}>
+      <BsChevronRight />
+    </div>
+  );
+};
+
+const PrevArrow = ({ onClick }) => {
+  return (
+    <div className="prevArrow" onClick={onClick}>
+      <BsChevronLeft />
+    </div>
+  );
+};
+
+const ImageSlider = ({ images, slidesToShow = 3 }) => {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    dots: false,
+    speed: 300,
+    slidesToShow: slidesToShow,
+    centerPadding: "0",
+    swipeToSlide: true,
+    focusOnSelect: true,
+    nextArrow: <NextArrow onClick />,
+    prevArrow: <PrevArrow onClick />,
+    beforeChange: (current, next) => setImageIndex(next),
+    responsive: [
+      {
+        breakpoint: 1490,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 820,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const templateImages = images.map((image, idx) => {
+    if (image !== null) {
+      return (
+        <div
+          className={idx === imageIndex ? "activeSlide" : "slide"}
+          key={image.id}
+        >
+          <div className="slideWrapper">
+            {image.code ? (
+              image.code
+            ) : (
+              <img className="image" src={image.src} alt={image.alt} />
+            )}
+          </div>
+          <div className="title">Name</div>
+          <div className="description">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </div>
+        </div>
+      );
+    }
+    return null;
+  });
+
+  return <Slider {...settings}>{templateImages}</Slider>;
+};
+
+export default ImageSlider;
